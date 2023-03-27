3ea5ddc96d7d2f3c4ee7e6a932e3859a55fa85b0
MarcoElia <marcoelia.masi@gmail.com>
2023-03-27 11:08:19 +0200

    T final version

--- src/esercizi/T.java
+++ src/esercizi/T.java
@@ -5,21 +5,20 @@ public class T {
     private int minuti;
     private int secondi;
 
-    public T(){
+    public T() {
 
     }
 
-    public T(int ore, int minuti, int secondi){
-        try{
-            if(ore < 0 || ore > 23 || minuti < 0 ||
-                    minuti > 59 || secondi <0 || secondi >59){
+    public T(int ore, int minuti, int secondi) {
+        try {
+            if (ore < 0 || ore > 23 || minuti < 0 ||
+                    minuti > 59 || secondi < 0 || secondi > 59) {
                 throw new Exception("Inserisci un orario valido");
             }
             this.ore = ore;
             this.minuti = minuti;
             this.secondi = secondi;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
@@ -38,60 +37,59 @@ public class T {
     }
 
     public void setOre(int ore) {
-        try{
-            if(ore < 0 || ore > 23){
+        try {
+            if (ore < 0 || ore > 23) {
                 throw new Exception("Inserisci un numero tra 0 e 23");
             }
             this.ore = ore;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
     }
 
     public void setMinuti(int minuti) {
-        try{
-            if(minuti < 0 || minuti > 59){
+        try {
+            if (minuti < 0 || minuti > 59) {
                 throw new Exception("Inserisci un numero tra 0 e 59");
             }
             this.minuti = minuti;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
     }
 
     public void setSecondi(int secondi) {
-        try{
-            if(secondi < 0 || secondi > 59){
+        try {
+            if (secondi < 0 || secondi > 59) {
                 throw new Exception("Inserisci un numero tra 0 e 59");
             }
             this.secondi = secondi;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void sottraiOrario(int ore, int minuti, int secondi){
+    public void sottraiOrario(int ore, int minuti, int secondi) {
         this.ore = this.ore - ore;
-        if(this.ore < 0){
+        if (this.ore < 0) {
             this.ore = 24 + this.ore;
         }
 
         this.minuti = this.minuti - minuti;
-        if(this.minuti < 0){
+        if (this.minuti < 0) {
             this.ore -= 1;
             this.minuti = 60 + this.minuti;
         }
 
         this.secondi = this.secondi - secondi;
-        if(this.secondi < 0){
+        if (this.secondi < 0) {
             this.minuti -= 1;
             this.secondi = 60 + this.secondi;
         }
+
+    }
 
 
     public void aggiungiOrario(int ore, int minuti, int secondi) throws IllegalArgumentException {
@@ -115,4 +113,5 @@ public class T {
         this.secondi = aggiungi_secondi;
 
     }
+
 }
