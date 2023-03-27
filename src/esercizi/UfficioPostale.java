0b47c588eeb4ab3995161e38a05bd7bff46709a0
Simone Munaò <simone.munao@nttdata.com>
2023-03-27 12:43:12 +0200

    eafl

--- src/esercizi/UfficioPostale.java
+++ src/esercizi/UfficioPostale.java
@@ -15,4 +15,14 @@ public class UfficioPostale {
         this.lettere = lettere;
     }
 
+    public int contaLetterePerDesƟnatario (String nome, String cognome){
+        int i = 0;
+        for (Lettera let: lettere){
+            if (let.getNomeDestinatario().equals(nome) &&
+                 let.getCognomeDestinatario().equals(cognome)){
+                i++;
+            }
+        }
+        return i;
+    }
 }
