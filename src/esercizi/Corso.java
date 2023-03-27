3132a117627d2db114100826e5a201374f67bd5f
Simone Munaò <simone.munao@nttdata.com>
2023-03-27 11:31:54 +0200

    ciao Marco

--- src/esercizi/Corso.java
+++ src/esercizi/Corso.java
@@ -29,6 +29,13 @@ public class Corso {
     public void setEsami(ArrayList<Esame> esami) {
         this.esami = esami;
     }
-
-
+    public int votoMinimo() {
+        int minimo = Integer.MIN_VALUE;
+        for (Esame esame : esami) {
+            if (esame.getVoto() == minimo) {
+                minimo = esame.getVoto();
+            }
+        }
+        return minimo;
+    }
 }
