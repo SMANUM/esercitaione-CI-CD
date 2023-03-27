28d571ff1aae4d98f009fa17176c00fbf5855774
Simone Munaò <simone.munao@nttdata.com>
2023-03-27 11:51:36 +0200

    ciao Marco_3.0

--- src/esercizi/Corso.java
+++ src/esercizi/Corso.java
@@ -5,11 +5,11 @@ public class Corso {
     private String nome;
     private ArrayList<Esame> esami;
 
-    public Corso(){
+    public Corso() {
 
     }
 
-    public Corso(String nome, ArrayList<Esame> esami){
+    public Corso(String nome, ArrayList<Esame> esami) {
         this.nome = nome;
         this.esami = esami;
     }
@@ -29,27 +29,26 @@ public class Corso {
     public void setEsami(ArrayList<Esame> esami) {
         this.esami = esami;
     }
+
     public int votoMinimo() {
-        int minimo = Integer.MIN_VALUE;
+        int minimo;
         for (Esame esame : esami) {
-            if (esame.getVoto() == minimo) {
+            if (esame.getVoto() < minimo) {
                 minimo = esame.getVoto();
             }
         }
         return minimo;
     }
-}
 
-    public ArrayList<String> studentiPeggiori(){
+    public ArrayList<String> studentiPeggiori() {
         int votoMinimo = 21;
         ArrayList<String> cognomiPeggiori = new ArrayList<String>();
-        for(int i = 0; i < esami.size(); i ++){
+        for (int i = 0; i < esami.size(); i++) {
             Esame esame = esami.get(i);
-            if(esame.getVoto() == votoMinimo){
+            if (esame.getVoto() == votoMinimo) {
                 cognomiPeggiori.add(esame.getCognome());
-            } 
+            }
         }
+        return cognomiPeggiori;
     }
-
-
 }
